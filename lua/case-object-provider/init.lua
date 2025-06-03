8f27fee790260de4fcd3ed623150899efe08a150
vE5li <ve5li@tuta.io>
2025-06-04 01:40:55 +0200

    Add `find_all`

--- lua/case-object-provider/init.lua
+++ lua/case-object-provider/init.lua
@@ -48,6 +48,140 @@ local function in_word(include_separator)
     }
 end
 
+local function add_word(objects, line, line_number, start_column, end_column, include_separator)
+    local word = line:sub(start_column, end_column)
+
+    if not word:find("[a-zA-Z]") then
+        -- This is just number
+        return
+    end
+
+    if word:find("[_-]") then
+        -- Separated by _ or -
+        local word_start = start_column
+        local offset = 0
+
+        if include_separator then
+            offset = 1
+        end
+
+        while start_column < end_column do
+            local current_character = line:sub(start_column, start_column)
+
+            if current_character == "_" or current_character == "-" then
+                -- This check is necessary for consecutive `_` or `-`
+                if word_start < start_column then
+                    table.insert(objects, {
+                        first_line = line_number,
+                        start_column = word_start,
+                        last_line = line_number,
+                        end_column = start_column - 1 + offset,
+                    })
+                end
+
+                -- TODO: We might want to include more that one character for the separator. That would support matches like:
+                --
+                -- text :  foo___bar
+                -- match:  ^    ^^ ^
+                --         |    |---
+                --         ------
+                --
+                -- Currently it looks like this
+                --
+                -- text :  foo___bar
+                -- match:  ^  ^  ^ ^
+                --         ----  ---
+                word_start = start_column + 1
+            end
+
+            start_column = start_column + 1
+        end
+
+        -- We need to add the last word to the objects. This will only not be hit if the last character is a '_' or '-'.
+        if word_start ~= start_column then
+            -- The last word is inserted differently. It uses the separator in before it rather than after it.
+            -- That way, selecting around a part will always include a separator.
+            table.insert(objects, {
+                first_line = line_number,
+                start_column = word_start - offset,
+                last_line = line_number,
+                end_column = start_column,
+            })
+        end
+    elseif word:find("[A-Z][a-z]") or word:find("[a-z][A-Z]") then
+        -- Separated by capital letters
+
+        local word_end = end_column
+        local waiting_for_uppercase = nil
+
+        while end_column >= start_column do
+            local current_character = line:sub(end_column, end_column)
+
+            if waiting_for_uppercase == nil then
+                waiting_for_uppercase = current_character:match("%u") == nil
+            elseif waiting_for_uppercase == true then
+                if current_character:match("%u") then
+                    -- We got an uppercase character and want to add this character and the tail to the objects.
+
+                    table.insert(objects, {
+                        first_line = line_number,
+                        start_column = end_column,
+                        last_line = line_number,
+                        end_column = word_end,
+                    })
+
+                    word_end = end_column - 1
+                    waiting_for_uppercase = nil
+                end
+            elseif waiting_for_uppercase == false then
+                if current_character:match("%U") then
+                    -- We got a lowercase character and want to add _only_ the tail to the objects.
+
+                    table.insert(objects, {
+                        first_line = line_number,
+                        start_column = end_column + 1,
+                        last_line = line_number,
+                        end_column = word_end,
+                    })
+
+                    word_end = end_column
+                    waiting_for_uppercase = nil
+                end
+            end
+
+            end_column = end_column - 1
+        end
+
+        -- We need to add the first word to the objects if it doesn't start with an uppercase letter.
+        if word_end > end_column then
+            table.insert(objects, {
+                first_line = line_number,
+                start_column = end_column + 1,
+                last_line = line_number,
+                end_column = word_end,
+            })
+        end
+    end
+end
+
+local function find_all(include_separator)
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    local objects = {}
+
+    for line_number, line in ipairs(lines) do
+        local offset = 1
+        local start_column, end_column = line:find("[a-zA-Z0-9_-]+", offset)
+
+        while start_column do
+            add_word(objects, line, line_number, start_column, end_column, include_separator)
+            offset = end_column + 1
+            start_column, end_column = line:find("[a-zA-Z0-9_-]+", offset)
+        end
+    end
+
+    return objects
+end
+
 local bindings = {
     {
         name = "case",
@@ -60,6 +194,7 @@ local bindings = {
             elseif requested == "next" then
             elseif requested == "last" then
             elseif requested == "every" then
+                return find_all(mode == "a")
             end
         end,
     },
